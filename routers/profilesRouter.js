79b4e4216c723e8c73d1baef9a0b14bb3e33f076
adamrodrigues11 <adamrodrigues11@gmail.com>
2022-11-15 13:35:29 -0800

    refactored profilesRouter and views

--- routers/profilesRouter.js
+++ routers/profilesRouter.js
@@ -1,35 +1,51 @@
+"use strict";
+
+const fs = require("fs").promises;
+const path = require("path");
+
 const express = require("express");
 const profilesRouter = express.Router();
 
-const fs = require("fs");
-const path = require("path");
-let profilesArr;
-
 const profilesPath = path.join(__dirname + "/../data/profiles.json");
 
-fs.promises.readFile(profilesPath) 
-    .then(contents => {
-    profilesArr = JSON.parse(contents);
-    console.log(profilesArr);
-    })
-
 profilesRouter.get("/", (req, res) => { 
-    const viewData = {
-        title: "Profiles",
-        profiles: profilesArr
-    };
-    res.render("profiles", viewData)
+    fs.readFile(profilesPath) 
+        .then(contents => {
+            const profiles = JSON.parse(contents);
+            const viewData = {
+                title: "Profiles",
+                profiles: profiles
+            };
+            res.render("profiles", viewData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.writeHead(500);
+            res.end("Error");
+        })
 });
 
 profilesRouter.get("/:id", (req, res) => {
-    const profile = profilesArr.find( p => p.id == req.params.id);
-    const viewData = {
-        title: profile.name,
-        profile: profile,
-        profiles: profilesArr,
-        layout: "./layouts/side-bar"
-    };
-    res.render("profile", viewData)
+    fs.readFile(profilesPath)
+        .then(contents => {
+            const profiles = JSON.parse(contents);
+            const profile = profiles.find( 
+                p => p.id === req.params.id);
+            const otherProfiles = profiles.filter(
+                p => p !== profile);
+            const viewData = {
+                title: profile.name,
+                profile: profile,
+                otherProfiles: otherProfiles,
+                layout: "./layouts/side-bar"
+            };
+            res.render("profile", viewData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.writeHead(500);
+            res.end("Error");
+        })
 });
 
 module.exports = profilesRouter;
