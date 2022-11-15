b77aa2fe03e4f26c8b207f45739777a89eb6546d
Mike <myamauchi@my.bcit.ca>
2022-11-15 13:34:08 -0800

    created apiRouter data and installed cors

--- app.js
+++ app.js
@@ -3,9 +3,13 @@
 const express = require("express");
 const path = require("path");
 const expressLayouts = require("express-ejs-layouts");
+const cors = require("cors");
 
 const app = express();
 const port = process.envPORT || 3000;
+
+// allow cross origin requests from any port on local machine
+app.use(cors({ origin: [/127.0.0.1*/, /localhost*/] }));
 
 // load Routers
 const indexRouter = require("./routers/indexRouter");
@@ -23,7 +27,7 @@ app.set("layout", "./layouts/full-width");
 
 app.use(indexRouter);
 app.use("/profiles", profilesRouter);
-// app.use("/api", apiRouter);
+app.use("/api", apiRouter);
 
 app.all("/*", (req, res) => {
     res.status(404).send("File Not Found");
