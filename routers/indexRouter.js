192b1af37900c0fbcb649809f6a8a52774198053
Catherine Addison <caddison4@my.bcit.ca>
2022-11-15 13:37:13 -0800

    updated contacts form & contact get/post in indexRouter

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -10,7 +10,22 @@ const path = require("path");
 const projectTitle = "Express Yourself"
 indexRouter.get("/", (req, res) => res.render("index",{title: `${projectTitle} - Home`}));  
 indexRouter.get("/about", (req, res) => res.render("about",{title: `${projectTitle} - About`}));
-indexRouter.get("/contact", (req, res) => res.render("contact",{title: `${projectTitle} - Contacts`}));
-indexRouter.post("/contact", (req, res) => res.send("Thanks for your submission"))
+
+
+
+indexRouter.get("/contact", (req, res) => {
+    res.render("contact", {
+        title: `${projectTitle} - Contacts`,
+        status: null,
+    });
+});
+
+indexRouter.post("/contact", (req, res) => {
+    res.render("contact", {
+        title: `${projectTitle} - Contacts`,
+        status: "received",
+        formData: req.body,
+    });
+});
 
 module.exports = indexRouter;
